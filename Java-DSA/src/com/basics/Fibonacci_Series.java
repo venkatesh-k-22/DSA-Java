75ea8351baa126cbb914e157a300b1a823dc56a4
Venkatesh K <venkateshk14046@gmail.com>
2024-10-30 21:25:44 +0530

    PRCH: how fibonacci logic works

--- Java-DSA/src/com/basics/Fibonacci_Series.java
+++ Java-DSA/src/com/basics/Fibonacci_Series.java
@@ -12,6 +12,10 @@ public class Fibonacci_Series {
     }
 
     static int fibonacciNumber(int n){
+
+        // Logic
+        // a = 0 1 1 2 3 5 8 13.....
+        // b = 1 1 2 3 5 8 13.....
         int count=2;
         int a=0;
         int b=1;

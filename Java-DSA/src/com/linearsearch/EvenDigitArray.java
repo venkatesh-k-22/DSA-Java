a2892d9ad29cffb31445ea42433c09585d996661
Venkatesh K <venkateshk14046@gmail.com>
2024-11-04 21:55:41 +0530

    PRCH: Find Even digit count in Array - Leet code solution.

---
+++ Java-DSA/src/com/linearsearch/EvenDigitArray.java
@@ -0,0 +1,22 @@
+package com.linearsearch;
+
+// Leet Code Problem - Find Numbers with Even Number of Digits
+public class EvenDigitArray {
+    public static void main(String[] args) {
+        int[] arr = {12,345,2,6,7896};
+        int res = findNumbers(arr);
+        System.out.println(res);
+    }
+    public static int findNumbers(int[] nums) {
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            int element = nums[i];
+            int count = (int)Math.log10(element)+1;
+
+            if(count%2==0){
+                res++;
+            }
+        }
+        return res;
+    }
+}
